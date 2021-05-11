a0be6928d6ad7b3860fbfc01be4c61359c770166
natpinnock <natpinnock@gmail.com>
2021-05-11 21:24:47 +0100

    images added

--- main.js
+++ main.js
@@ -8,12 +8,25 @@ function handleFoodChange() {
   foodToSearch = document.querySelector("#food-input").value;
 }
 
+let data = "";
+let recipeSection = "";
+let recipeLabel = "";
 async function fetchRecipe(food) {
   let response = await fetch(
     `https://api.edamam.com/search?q=${foodToSearch}&app_id=3424b541&app_key=0e5519f0352e931ba2358451a65bc487`
   );
-  let data = await response.json();
-  let recipeLabel = document.querySelector("#recipe-label");
-  recipeLabel.innerHTML = data.hits[0].recipe.label;
-  recipeLabel.href = data.hits[0].recipe.url;
+  data = await response.json();
+  for (i = 0; i < data.hits.length; i++) {
+    recipeSection = document.querySelector("#recipe-section");
+    recipeLabel = document.createElement("a");
+    recipeLabel.innerHTML = data.hits[i].recipe.label;
+    recipeLabel.href = data.hits[i].recipe.url;
+    recipeSection.appendChild(recipeLabel);
+    recipeImage = document.createElement("img");
+    recipeImage.src = data.hits[i].recipe.image;
+    recipeSection.appendChild(recipeImage);
+    recipeIngredients = document.createElement("img");
+    recipeImage.src = data.hits[i].recipe.image;
+    recipeSection.appendChild(recipeImage);
+  }
 }
