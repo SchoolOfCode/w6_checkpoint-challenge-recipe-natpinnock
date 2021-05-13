b86b792a9a4a84cba2f5ce1c6d08b4f044ded92a
natpinnock <natpinnock@gmail.com>
2021-05-13 06:29:11 +0100

    display first 5 results

--- main.js
+++ main.js
@@ -16,17 +16,19 @@ async function fetchRecipe(food) {
     `https://api.edamam.com/search?q=${foodToSearch}&app_id=3424b541&app_key=0e5519f0352e931ba2358451a65bc487`
   );
   data = await response.json();
-  for (i = 0; i < data.hits.length; i++) {
+  for (i = 0; i < 8; i++) {
     recipeSection = document.querySelector("#recipe-section");
+    recipeBox = document.createElement("section");
+    recipeBox.id = "recipebox";
+    recipeSection.appendChild(recipeBox);
     recipeLabel = document.createElement("a");
     recipeLabel.innerHTML = data.hits[i].recipe.label;
     recipeLabel.href = data.hits[i].recipe.url;
-    recipeSection.appendChild(recipeLabel);
+    recipeBox.appendChild(recipeLabel);
     recipeImage = document.createElement("img");
     recipeImage.src = data.hits[i].recipe.image;
-    recipeSection.appendChild(recipeImage);
-    recipeIngredients = document.createElement("img");
-    recipeImage.src = data.hits[i].recipe.image;
-    recipeSection.appendChild(recipeImage);
+    recipeBox.appendChild(recipeImage);
   }
 }
+
+//delete current items when a new search starts
